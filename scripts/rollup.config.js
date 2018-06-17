cfe7cbf74009d0ea07db8bb3a361c8b02fe47c3a
Bowen <mr_lbw@outlook.com>
2018-06-18 00:45:44 +0800

    build: rollup-babel

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,19 +1,35 @@
-// import babel from 'rollup-plugin-babel'
-import alias from 'rollup-plugin-alias'
-import { terser } from 'rollup-plugin-terser'
+const path = require('path')
+const version = process.env.VERSION || require('../package.json').version
+const alias = require('rollup-plugin-alias')
+const babel = require('rollup-plugin-babel')
+const { terser } = require('rollup-plugin-terser')
+
+const banner = 
+  '/*!\n' +
+  ' * jsonp v' + version + '\n' +
+  ' * Copyrights (c) 2018-' + new Date().getFullYear() + ' Bowen (lbwa)\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+const resolve = p => path.resolve(__dirname, '../', p)
 
 export default {
-  input: 'index.js',
+  input: resolve('src/index.js'),
   output: {
-    file: 'dist/bundle.js',
+    file: resolve('dist/bundle.min.js'),
     name: 'jsonp',
-    format: 'umd'
+    format: 'umd',
+    banner
   },
   plugins: [
     alias(),
-    terser()
-    // babel({
-    //   exclude: 'node_modules/**'
-    // })
+    terser({
+      output: {
+        comments: /^!/
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
   ]
 }
