7d27c89f5df15c85280f1bb6b3f4ae02c1a1a9d8
Bowen <mr_lbw@outlook.com>
2018-06-18 15:34:20 +0800

    debug: 解决浏览器直接引用问题

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const version = process.env.VERSION || require('../package.json').version
 const alias = require('rollup-plugin-alias')
 const babel = require('rollup-plugin-babel')
+const replace = require('rollup-plugin-replace')
 const { terser } = require('rollup-plugin-terser')
 
 const banner = 
@@ -13,37 +14,61 @@ const banner =
 
 const resolve = p => path.resolve(__dirname, '../', p)
 
-const isProduction = process.env.NODE_ENV === 'production'
-
-const config = {
-  input: resolve('lib/index.js'),
-  output: {
-    file: resolve(isProduction ? 'dist/better-jsonp.min.js' : 'dist/better-jsonp.js'),
-    name: 'jsonp',
-    format: isProduction ? 'umd' : 'cjs',
-    banner
+const builds = {
+  'development': {
+    entry: resolve('lib/index.js'),
+    dest: resolve('dist/better-jsonp.js'),
+    format: 'umd',
+    banner,
+    plugins: []
   },
-  plugins: [
-    // define aliases and extension which should be resolved
-    alias({
-      utils: resolve('lib/utils') // url alias, eg. utils -> ../utils
-    }),
-    // rollup-plugin-replace, be used to inject environment variable to output
-    // replace({
-    //   exclude: 'node_modules/**',
-    //   ENVIRONMENT: JSON.stringify(process.env.NODE_ENV)
-    // }),
-    // convert to lower ES version
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    // minify output
-    isProduction && terser({
-      output: {
-        comments: /^!/
-      }
-    })
-  ]
+  'production': {
+    entry: resolve('lib/index.js'),
+    dest: resolve('dist/better-jsonp.min.js'),
+    format: 'umd',
+    banner,
+    plugins: [
+      terser({
+        output: {
+          comments: /^!/
+        }
+      })
+    ]
+  }
 }
 
-export default config
+function genConfig (env) {
+  const options = builds[env]
+  const config = {
+    input: options.entry,
+    output: {
+      file: options.dest,
+      format: options.format,
+      banner: options.banner,
+
+      // module name in global env. eg. window.JSONP = function () {...}
+      name: options.moduleName || 'JSONP'
+    },
+    plugins: [
+      // define aliases and extension which should be resolved
+      alias({
+        utils: resolve('lib/utils') // url alias, eg. utils -> ../utils
+      }),
+      // rollup-plugin-replace, be used to inject environment variable to output
+      replace({
+        exclude: 'node_modules/**',
+        ENVIRONMENT: JSON.stringify(process.env.NODE_ENV)
+      }),
+      // convert to lower ES version
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      ...options.plugins
+    ],
+  }
+
+  return config
+}
+
+// rollup -c scripts/rollup.config.js --environment NODE_ENV:production
+module.exports = genConfig(process.env.NODE_ENV)
