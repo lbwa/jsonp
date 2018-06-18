be1cd4e9c1f5825d29e0aabdedf0227e33612bb1
Bowen <mr_lbw@outlook.com>
2018-06-18 17:34:58 +0800

    debug: publish.sh

--- publish.sh
+++ publish.sh
@@ -4,12 +4,6 @@ set -e
 
 # 实现方法 1
 
-read -p "请输入提交至 Git 仓库的提交描述信息：" message
-
-git add .
-git commit -m "$message"
-git push origin master
-
 read -p "升级版本号:
 1. [patch] 修复了 BUG，并向下兼容
 2. [minor] 添加了一些新功能，并向下兼容
@@ -21,34 +15,19 @@ yarn run build:dev
 
 # only works for yarn v1.7.0+
 # It will also tag in git
-# yarn version --patch
+yarn version --$var
 
-npm version $var
+# CURRENT_VERSION=`cat package.json | grep version | sed -E 's/[^.0-9]//g'`
+CURRENT_VERSION=`node -p "require('./package.json').version"`
 
-npm publish
+yarn publish --new-version $CURRENT_VERSION
 
 git push --tag
 
-# 实现方法 2
+read -p "请输入提交至 Git 仓库的提交描述信息：" message
 
-# verify parameter number
-#　-ne means not equal
-# if [ "$#" -ne "1" ]
-# then
-#   echo '请在双引号内输入更新版本的方式。yarn run publish "[--patch] [--minor] [--major]"'
-#   # code 2 means wrong usage
-#   exit 2
-# else
-
-# yarn run build
-# yarn run build:dev
-
-# # only works for yarn v1.7.0+
-# # It will also tag in git
-# yarn version $1
-
-# yarn publish
-
-# fi
+git add .
+git commit -m "$message"
+git push origin master
 
 echo ' -- Mission completed !! -- '
