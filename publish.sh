018670e500d9e5ab4d500422bca20376e1c84a56
Bowen <mr_lbw@outlook.com>
2018-06-20 23:15:18 +0800

    build: optimize publication process

--- publish.sh
+++ publish.sh
@@ -10,19 +10,24 @@ read -p "升级版本号:
 3. [major] 进行了破坏性更新，不兼容之前的版本
 请选择升级版本号的方式：" var
 
-# only works for yarn v1.7.0+
-# It will run `git tag -a v1.0.0 -m 'info'` at same time
-yarn version --$var
+# https://yarnpkg.com/en/docs/cli/version#toc-git-tags
+# disable the git tagging by yarn default behavior
+# yarn config set version-git-tag true
 
+yarn version --$var
 yarn run build
 
 # CURRENT_VERSION=`cat package.json | grep version | sed -E 's/[^.0-9]//g'`
 CURRENT_VERSION=`node -p "require('./package.json').version"`
 
 yarn publish --new-version $CURRENT_VERSION
-
 yarn run commit
 
-git push --tag
-
+# To include the latest code, it must be invoked after commit
+git tag v$CURRENT_VERSION
+git push
+git push origin --tags
 echo ' <<<<<<<<<< Mission completed >>>>>>>>>> '
+
+# more details:
+# https://github.com/lbwa/lbwa.github.io/issues/17
