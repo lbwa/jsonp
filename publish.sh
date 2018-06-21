411e20c7b5c678a53e67a3defa401ab136f35f9d
Bowen <mr_lbw@outlook.com>
2018-06-21 12:48:57 +0800

    build: optimize publication process

--- publish.sh
+++ publish.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 yarn run lint
+yarn run test
 
-read -p "升级版本号:
-1. [patch] 修复了 BUG，并向下兼容
-2. [minor] 添加了一些新功能，并向下兼容
-3. [major] 进行了破坏性更新，不兼容之前的版本
-请选择升级版本号的方式：" var
+# recommend Semantic Versioning 2.0.0 , https://semver.org/
+read -p "Upgrade version:
+1. [patch] when you make backwards-compatible bug fixes.
+2. [minor] when you add functionality in a backwards-compatible manner.
+3. [major] when you make incompatible API changes.
+Your choice：" var
 
+# upgrade package
 # https://yarnpkg.com/en/docs/cli/version#toc-git-tags
 # disable the git tagging by yarn default behavior
 # yarn config set version-git-tag true
@@ -17,17 +20,25 @@ read -p "升级版本号:
 yarn version --$var
 yarn run build
 
+# publish package
 # CURRENT_VERSION=`cat package.json | grep version | sed -E 's/[^.0-9]//g'`
 CURRENT_VERSION=`node -p "require('./package.json').version"`
-
 yarn publish --new-version $CURRENT_VERSION
-yarn run commit
 
-# To include the latest code, it must be invoked after commit
-git tag v$CURRENT_VERSION
+# make sure the latest code has been included in the latest commit before git tagging
+yarn run commit
 git push
+
+# git tagging must be invoked after new commit
+git tag v$CURRENT_VERSION
 git push origin --tags
-echo ' <<<<<<<<<< Mission completed >>>>>>>>>> '
+
+# generate and commit CHANGELOG which is based on tags log and commit log
+yarn run changelog
+yarn run commit
+git push
+
+echo ' ============ Mission completed ============ '
 
 # more details:
 # https://github.com/lbwa/lbwa.github.io/issues/17
